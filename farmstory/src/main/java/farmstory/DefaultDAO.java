a9552e17755125acd2fc2738832b0d53ccecefc4
id3ntity99 <jas06113@gmail.com>
2025-02-24 11:00:06 +0900

    Deprecated DefaultDAO

--- farmstory/src/main/java/farmstory/DefaultDAO.java
+++ farmstory/src/main/java/farmstory/DefaultDAO.java
@@ -3,6 +3,10 @@ package farmstory;
 import java.util.List;
 import farmstory.util.ConnectionHelper;
 
+/**
+ * @deprecated "Entity마다 사용하는 Query가 다르므로 본 클래스를 활용하기에는 어려움이 있음"
+ */
+@Deprecated
 public class DefaultDAO<T extends DataTransferObject> implements DataAccessObject<T> {
   protected final ConnectionHelper helper;
 
@@ -12,31 +16,26 @@ public class DefaultDAO<T extends DataTransferObject> implements DataAccessObjec
 
   @Override
   public void insert(T dto) throws DataAccessException {
-    // TODO Auto-generated method stub
 
   }
 
   @Override
   public T select(T dto) throws DataAccessException {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public List<T> selectAll() throws DataAccessException {
-    // TODO Auto-generated method stub
     return null;
   }
 
   @Override
   public void update(T dto) throws DataAccessException {
-    // TODO Auto-generated method stub
 
   }
 
   @Override
   public void delete(T dto) throws DataAccessException {
-    // TODO Auto-generated method stub
 
   }
 }
