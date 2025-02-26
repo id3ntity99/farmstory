438b704b8d2a55e1101815c27240dbd7e903c81b
Jason Lee <jas06113@gmail.com>
2025-02-27 08:44:51 +0900

    implementing a contorller for signing up

--- farmstory/src/main/java/farmstory/controller/user/SignUpController.java
+++ farmstory/src/main/java/farmstory/controller/user/SignUpController.java
@@ -4,33 +4,45 @@ import java.io.IOException;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.google.gson.JsonObject;
 import farmstory.CountableDAO;
 import farmstory.dao.UserDAO;
 import farmstory.dto.UserDTO;
 import farmstory.exception.DataAccessException;
 import farmstory.service.CountableDefaultService;
 import farmstory.util.ConnectionHelper;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class SignInController extends HttpServlet {
+@WebServlet("/signup")
+public class SignUpController extends HttpServlet {
   private static final long serialVersionUID = UUID.randomUUID().version();
-  private static final Logger LOGGER = LoggerFactory.getLogger(SignInController.class.getName());
+  private static final Logger LOGGER = LoggerFactory.getLogger(SignUpController.class.getName());
 
   private CountableDAO<UserDTO> dao;
   private CountableDefaultService<UserDTO> service;
 
   @Override
   public void init() throws ServletException {
-    this.dao = new UserDAO(new ConnectionHelper("jdbc/farmstory"));
+    this.dao = new UserDAO(new ConnectionHelper("jdbc/Farmstory"));
     this.service = new CountableDefaultService<>(dao);
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/user/login.jsp");
+    dispatcher.forward(req, resp);
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+    LOGGER.info("Potential user accessed /signin...Checking form data");
     String id = req.getParameter("id");
     String password = req.getParameter("password");
     String name = req.getParameter("name");
@@ -51,11 +63,21 @@ public class SignInController extends HttpServlet {
     try {
       int idCount = service.count();
       if (idCount > 0) {
-        // TODO: Send errorneous response.
+        JsonObject json = new JsonObject();
+        json.addProperty("isSuccess", false);
+        json.addProperty("message", "Duplicate ID detected");
+        resp.setStatus(HttpServletResponse.SC_CONFLICT);
+        resp.setContentType("application/json");
+        resp.getWriter().println(json);
+      } else {
+        service.create(dto);
       }
       service.create(dto);
-    } catch (DataAccessException e) {
-      // TODO: Send 500 response.
+      RequestDispatcher dispatcher = req.getRequestDispatcher("/");
+      dispatcher.forward(req, resp);
+    } catch (DataAccessException | IOException e) {
+      LOGGER.error("While handling user information, exception raised", e);
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
   }
 }
