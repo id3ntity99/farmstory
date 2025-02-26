352b548bcc258dbfe0bec05c30a35e78df6fabe2
id3ntity99 <jas06113@gmail.com>
2025-02-26 14:00:21 +0900

    Utility class that validates user input

--- farmstory/src/main/java/farmstory/util/InputValidator.java
+++ farmstory/src/main/java/farmstory/util/InputValidator.java
@@ -1,6 +1,8 @@
-package farmstory.filter;
+package farmstory.util;
 
-public class ValidInputRegex {
+import farmstory.exception.InvalidUserInformationException;
+
+public class InputValidator {
   public static final String ID_REGEX = "/^[a-z]+[a-z0-9]{4,19}$/g";
   public static final String PASSWORD_REGEX =
       "/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\\\(\\\\)\\-_=+]).{5,16}$/";
@@ -12,4 +14,14 @@ public class ValidInputRegex {
   public static final String ZIP_REGEX = "/(\\d{3}-\\d{3}|\\d{5})/";
   public static final String ADDR_REGEX = "/[가-힣 1-9\\-]+/g";
 
+  public static void validate(String input, String regex) throws InvalidUserInformationException {
+    if (input == null || !input.matches(regex)) {
+      String message = String.format("Invalid user input detected: %s", input);
+      throw new InvalidUserInformationException(message);
+    }
+  }
+
+  private InputValidator() {
+    // Empty constructor.
+  }
 }
