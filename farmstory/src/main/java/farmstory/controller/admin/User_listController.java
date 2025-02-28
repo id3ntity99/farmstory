0d024945efe2a5cf2134458512fad0f4838c07e7
minsung <dpsdy1@gmail.com>
2025-02-28 12:14:39 +0900

    servlet 등록

---
+++ farmstory/src/main/java/farmstory/controller/admin/User_listController.java
@@ -0,0 +1,28 @@
+package farmstory.controller.admin;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/user-list.do")
+public class User_listController extends HttpServlet{
+
+	private static final long serialVersionUID = -4296729693079948400L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/user-list.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
+
+}
