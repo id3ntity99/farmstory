abb68cd93eb0641f14672a2bbc3f6f5f00c1b66a
id3ntity99 <jas06113@gmail.com>
2025-02-28 17:51:48 +0900

    Using request wrapper to use inputstrea/reader multiple times

--- farmstory/src/main/java/farmstory/filter/UserInputValidator.java
+++ farmstory/src/main/java/farmstory/filter/UserInputValidator.java
@@ -19,6 +19,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns = {"/signup"})
@@ -52,18 +53,19 @@ public class UserInputValidator extends HttpFilter {
     validator.validate("email", InputValidator.EMAIL_REGEX);
     validator.validate("phoneNum", InputValidator.PHONE_NUM_REGEX);
     validator.validate("zip", InputValidator.ZIP_REGEX);
-    validator.validate("address", InputValidator.ADDR_REGEX);
-    validator.validate("addressDetail", InputValidator.ADDR_REGEX);
-    System.out.println(obj.asMap().get("addressDetail").getAsString());
+    // TODO Resolve validation of address and addressDetail with regex
+    // validator.validate("address", InputValidator.ADDR_REGEX);
+    // validator.validate("addressDetail", InputValidator.ADDR_DETAIL_REGEX);
     validator.confirmPassword();
   }
 
   @Override
   protected void doFilter(HttpServletRequest request, HttpServletResponse response,
       FilterChain chain) throws IOException, ServletException {
-    if (request.getMethod().equalsIgnoreCase("post")) {
+    HttpServletRequestWrapper wrappedRequest = new CachedHttpServletRequest(request);
+    if (wrappedRequest.getMethod().equalsIgnoreCase("post")) {
       // Parse JSON string to JSON object
-      String jsonString = stringify(request.getReader());
+      String jsonString = stringify(wrappedRequest.getReader());
       JsonObject obj = JsonParser.parseString(jsonString).getAsJsonObject();
 
       // Iterate each property to check if there are null values;
@@ -74,15 +76,14 @@ public class UserInputValidator extends HttpFilter {
         // Validate user info data such as ID, password, etc.
         try {
           validate(obj);
-          chain.doFilter(request, response);
+          chain.doFilter(wrappedRequest, response);
         } catch (InvalidUserInformationException | PasswordConfirmException e) {
-          LOGGER.warn(e.getMessage(), e);
-          e.printStackTrace();
+          LOGGER.debug(e.getMessage(), e);
           response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
       }
     } else {
-      chain.doFilter(request, response);
+      chain.doFilter(wrappedRequest, response);
     }
   }
 }
