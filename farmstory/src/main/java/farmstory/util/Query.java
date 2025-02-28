c1144ac3dc3db9669f44ac269dbd88f093397278
id3ntity99 <jas06113@gmail.com>
2025-02-28 17:52:58 +0900

    Password encryption function is added to query

--- farmstory/src/main/java/farmstory/util/Query.java
+++ farmstory/src/main/java/farmstory/util/Query.java
@@ -2,7 +2,7 @@ package farmstory.util;
 
 public final class Query {
   public static final String INSERT_USER =
-      "INSERT INTO `User` VALUES(?,?,?,?,0,0,?,?,?,?,?,NOW(),NULL);";
+      "INSERT INTO `User` VALUES(?,SHA2(?, 256),?,?,0,0,?,?,?,?,?,NOW(),NULL);";
 
   private Query() {
     // Empty constructor
