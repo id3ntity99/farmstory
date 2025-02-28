45f049694d34f969a7c84c75c9452ebbcd6347ff
minsung <dpsdy1@gmail.com>
2025-02-28 12:19:39 +0900

    servlet 등록

---
+++ farmstory/src/main/java/farmstory/controller/admin/Order_listController.java
@@ -0,0 +1,27 @@
+package farmstory.controller.admin;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/order-list.do")
+public class Order_listController extends HttpServlet{
+
+	private static final long serialVersionUID = 161266416260600026L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/order-list.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+
+}
