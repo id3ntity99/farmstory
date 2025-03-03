4b3d01730ae89fe6f22d25131b63b26c8bbf534b
Jason Lee <jas06113@gmail.com>
2025-03-04 02:17:14 +0900

    Chagned count query string format

--- farmstory/src/main/java/farmstory/util/Query.java
+++ farmstory/src/main/java/farmstory/util/Query.java
@@ -2,7 +2,8 @@ package farmstory.util;
 
 public final class Query {
   public static final String INSERT_USER =
-      "INSERT INTO `User` VALUES(?,SHA2(?, 256),?,?,0,0,?,?,?,?,?,NOW(),NULL);";
+      "INSERT INTO `user` VALUES(?,SHA2(?, 256),?,?,0,0,?,?,?,?,?,NOW(),NULL);";
+  public static final String COUNT_USER = "SELECT COUNT(`?`) FROM `user` WHERE `id` = ?;";
 
   private Query() {
     // Empty constructor
