c47e114c7a562a4882ed4798207f1588a6f3fa15
id3ntity99 <jas06113@gmail.com>
2025-03-05 12:45:30 +0900

    Implemented terms page

--- farmstory/src/main/java/farmstory/util/Query.java
+++ farmstory/src/main/java/farmstory/util/Query.java
@@ -5,6 +5,12 @@ public final class Query {
       "INSERT INTO `user` VALUES(?,SHA2(?, 256),?,?,0,0,?,?,?,?,?,NOW(),NULL);";
   public static final String COUNT_USER = "SELECT COUNT(?) FROM `user` WHERE `id` = ?;";
 
+  public static final String INSERT_TERM = "INSERT INTO `Term` VALUES(?, ?, ?);";
+  public static final String SELECT_TERM = "SEELCT * FROM `Term` WHERE `id` = ?;";
+  public static final String SELECT_ALL_TERMS = "SELECT * FROM `Term`;";
+  public static final String UPDATE_TERM = "UPDATE `Term` SET `title`=? `content`=? WHERE `id`=?;";
+  public static final String DELETE_TERM = "DELETE FROM `Term` WHERE `id` = ?;";
+
   private Query() {
     // Empty constructor
   }
