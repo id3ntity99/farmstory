73fd3a825ef5d8bfdad2e4048322de522330951b
id3ntity99 <jas06113@gmail.com>
2025-03-06 15:51:27 +0900

    Deleted duplicated code with same logic

--- farmstory/src/main/java/farmstory/util/Query.java
+++ farmstory/src/main/java/farmstory/util/Query.java
@@ -18,6 +18,9 @@ public final class Query {
   public static final String UPDATE_TERM = "UPDATE `term` SET `title`=? `content`=? WHERE `id`=?;";
   public static final String DELETE_TERM = "DELETE FROM `term` WHERE `id` = ?;";
 
+  public static final String INSERT_PROD_IMAGE =
+      "INSERT INTO `product_image` (`product_id`, `thumbnail_location`, `info_location`, `detail_location`) VALUES (?,?,?,?)";
+
   private Query() {
     // Empty constructor
   }
