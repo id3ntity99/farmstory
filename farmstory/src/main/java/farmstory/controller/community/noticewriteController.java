ad4ae1e2d310e9a48b75a3b637a5327176191d24
wnsgur0028 <wnsgur0028@gmail.com>
2025-03-06 17:40:41 +0900

    페이지연결

--- farmstory/src/main/java/farmstory/controller/community/noticewriteController.java
+++ farmstory/src/main/java/farmstory/controller/community/noticewriteController.java
@@ -7,27 +7,38 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import farmstory.controller.article.ListControllerGardening;
+import farmstory.dao.ArticleDAO;
+import farmstory.dto.ArticleDTO;
+import farmstory.exception.DataAccessException;
+import farmstory.service.CountableDefaultService;
+import farmstory.service.DefaultService;
+import farmstory.util.ConnectionHelper;
 
 
-@WebServlet("/CommunityWrite.do")
-public class writeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+@WebServlet("/noticeWrite.do")
+public class noticewriteController extends HttpServlet {
 
-    public writeController() {
+    @Override
+    public void init() throws ServletException {
 
     }
 
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//Viewport
-				RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/community/write.jsp");
+				RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/community/noticewrite.jsp");
 				dispatcher.forward(req, resp);
 	}
 
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 	}
+	
 }
