046b1e4b2f7d2243c389263f531ece2aba1f23c0
Jason Lee <jas06113@gmail.com>
2025-03-07 03:58:26 +0900

    Modified dummy data creation script

--- sql/farmstory-dummy.sql
+++ sql/farmstory-dummy.sql
@@ -4,21 +4,23 @@ INSERT INTO `user` VALUES ("abc123", SHA2("abc@123", 256), "김철수", "뭘봐"
 
 INSERT INTO company VALUES (1, "화북과수원", "홍길동", "010-1234-5677", "경상북도 상주시 화북면");
 
+DESCRIBE product_image;
+INSERT INTO product_image VALUES (1, "/home/images/thumbnail/thumb1.jpg", "/home/images/info/info1.jpg", "/home/images/detail/detail1.jpg");
+INSERT INTO product_image VALUES (2, "/home/images/thumbnail/thumb2.jpg", "/home/images/info/info2.jpg", "/home/images/detail/detail2.jpg");
+INSERT INTO product_image VALUES (3, "/home/images/thumbnail/thumb3.jpg", "/home/images/info/info3.jpg", "/home/images/detail/detail3.jpg");
+INSERT INTO product_image VALUES (4, "/home/images/thumbnail/thumb4.jpg", "/home/images/info/info4.jpg", "/home/images/detail/detail4.jpg");
+INSERT INTO product_image VALUES (5, "/home/images/thumbnail/thumb5.jpg", "/home/images/info/info5.jpg", "/home/images/detail/detail5.jpg");
+SELECT * FROM product_image;
+
 DESCRIBE product;
 INSERT INTO product VALUES (1, 1, "사과 100g", "과일", 4000, 4, 1.0, 2500, 23, 2, NOW());
 INSERT INTO product VALUES (2, 1, "사과 200g", "과일", 5000, 5, 2.0, 2500, 33, 3, NOW());
 INSERT INTO product VALUES (3, 1, "사과 300g", "과일", 6000, 6, 3.0, 2500, 43, 4, NOW());
 INSERT INTO product VALUES (4, 1, "사과 400g", "과일", 7000, 7, 4.0, 2500, 13, 5, NOW());
 INSERT INTO product VALUES (5, 1, "사과 500g", "과일", 8000, 8, 5.0, 2500, 73, 2, NOW());
-SELECT * FROM product;
+INSERT INTO `product` (`company_id`, `name`, `category`, `price`, `point`, `discount_rate`, `delivery_fee`, `stock`, `image_id`, `register_date`) VALUES (1, "사과 600g", "과일", 9000,9,6.0,2500,71,2, NOW());
 
-DESCRIBE product_image;
-INSERT INTO product_image VALUES (1, 1, "/home/images/thumbnail/thumb1.jpg", "/home/images/info/info1.jpg", "/home/images/detail/detail1.jpg");
-INSERT INTO product_image VALUES (2, 2, "/home/images/thumbnail/thumb2.jpg", "/home/images/info/info2.jpg", "/home/images/detail/detail2.jpg");
-INSERT INTO product_image VALUES (3, 3, "/home/images/thumbnail/thumb3.jpg", "/home/images/info/info3.jpg", "/home/images/detail/detail3.jpg");
-INSERT INTO product_image VALUES (4, 4, "/home/images/thumbnail/thumb4.jpg", "/home/images/info/info4.jpg", "/home/images/detail/detail4.jpg");
-INSERT INTO product_image VALUES (5, 5, "/home/images/thumbnail/thumb5.jpg", "/home/images/info/info5.jpg", "/home/images/detail/detail5.jpg");
-SELECT * FROM product_image;
+SELECT * FROM product;
 
 INSERT INTO article VALUES (1, "abc123", "글쓰기 테스트1", "뭘봐", "글쓰기 테스트 입니다1", 1, 1, 0, NOW());
 INSERT INTO article VALUES (2, "abc123", "글쓰기 테스트2", "뭘봐", "글쓰기 테스트 입니다2", 1, 1, 0, NOW());
